b0973e3bf254183f139d1c164387e2f9cc0405da
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-11-21 19:41:03 +0100

    feat(backend): Add CORS middleware

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import passport from "passport";
 import "./config/passportConfig";
+import cors from "cors";
 import router from "./routes/Router";
 import userRouter from "./routes/UserRouter";
 import postRouter from "./routes/PostRouter";
@@ -13,6 +14,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(passport.initialize());
+app.use(cors());
 
 // Routes
 app.use("/hello-world", router);
