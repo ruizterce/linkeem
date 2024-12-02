bef7e6296d19ad7b7d456834d0a9c03e028f881c
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-12-02 20:51:54 +0100

    feat(feed): add placeholder message for empty feed
    - Added a conditional rendering for the feed page to display a
    placeholder message when there are no posts in the feed
    - Included a clickable area with ripple effect that navigates users to
    the Discover page to find new content

--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { IonContent, IonPage } from "@ionic/react";
+import {
+  IonContent,
+  IonPage,
+  IonRippleEffect,
+  useIonRouter,
+} from "@ionic/react";
 import MainHeader from "../components/MainHeader";
 import { fetchFollowedPosts } from "../api/post";
 import { usePosts } from "../hooks/usePosts";
@@ -7,16 +12,32 @@ import PostList from "../components/PostList";
 
 const Feed: React.FC = () => {
   const { posts, hasMore, loadPosts } = usePosts(fetchFollowedPosts);
+  const router = useIonRouter();
 
   return (
     <IonPage>
       <MainHeader title="Feed" />
       <IonContent className="ion-padding" fixedSlotPlacement="before">
-        <PostList
-          posts={posts}
-          loadMore={() => loadPosts()}
-          hasMore={hasMore}
-        />
+        {posts.length > 0 ? (
+          <PostList
+            posts={posts}
+            loadMore={() => loadPosts()}
+            hasMore={hasMore}
+          />
+        ) : (
+          <div className="h-full flex flex-col items-center justify-center">
+            <div
+              className="py-20 px-6 text-center rounded-lg ion-activatable ripple-parent border-2 border-solid border-gray-100 hover:border-medium text-sm text-gray-600 dark:text-gray-300"
+              onClick={() => {
+                router.push("/discover", "forward");
+              }}
+            >
+              <p>Your Feed has no posts yet!</p>
+              <p>Click here to Discover new content!</p>
+              <IonRippleEffect className="rounded-md"></IonRippleEffect>
+            </div>
+          </div>
+        )}
       </IonContent>
     </IonPage>
   );
